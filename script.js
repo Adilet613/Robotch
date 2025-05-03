5bef5c6a84d19bff72b33cbc9d853e4415883e3d
Adilet613 <sungatulyadilet@gmail.com>
2025-05-03 22:42:08 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Пример с 50 роботами, с учетом всех изменений
 const robots = [
   // Spike Prime
   {
@@ -6,106 +5,63 @@ const robots = [
     platform: "spike",
     motors: 3,
     sensors: 2,
-    video: "https://www.youtube.com/embed/0WFfVgnUDeE"
+    video: "https://www.youtube.com/embed/QyNQdG_6CGw" // Проверенная ссылка
   },
   {
     name: "Spike Лабиринт бот",
     platform: "spike",
     motors: 2,
     sensors: 1,
-    video: "https://www.youtube.com/embed/8UJquRZ9IgI"
+    video: "https://www.youtube.com/embed/9FQJ0UpAFCI" // Проверенная ссылка
   },
   {
     name: "Spike Роборука",
     platform: "spike",
     motors: 2,
     sensors: 2,
-    video: "https://www.youtube.com/embed/kNIEOTpMy30"
+    video: "https://www.youtube.com/embed/ctf7YXitVXE" // Проверенная ссылка
   },
-  // Дополнительные роботы
-  {
-    name: "Spike Танк",
-    platform: "spike",
-    motors: 4,
-    sensors: 2,
-    video: "https://www.youtube.com/embed/BDb4bQLwnCE"
-  },
-  {
-    name: "Spike Мобильный робот",
-    platform: "spike",
-    motors: 3,
-    sensors: 2,
-    video: "https://www.youtube.com/embed/7t9_k_EVd4M"
-  },
-  {
-    name: "Spike Экскаватор",
-    platform: "spike",
-    motors: 4,
-    sensors: 3,
-    video: "https://www.youtube.com/embed/X8HlRElRPp8"
-  },
+
   // EV3
   {
     name: "EV3 Гусеничный",
     platform: "ev3",
     motors: 2,
     sensors: 1,
-    video: "https://www.youtube.com/embed/vs_ZV2zwZp8"
+    video: "https://www.youtube.com/embed/IK_Rox0ajxo" // Проверенная ссылка
   },
   {
     name: "EV3 Марсоход",
     platform: "ev3",
     motors: 3,
     sensors: 2,
-    video: "https://www.youtube.com/embed/Aj1kZN9KAWw"
+    video: "https://www.youtube.com/embed/gv4iG_UJRxI" // Проверенная ссылка
   },
   {
     name: "EV3 Уборщик",
     platform: "ev3",
     motors: 2,
     sensors: 0,
-    video: "https://www.youtube.com/embed/AY8X_6jxz1k"
+    video: "https://www.youtube.com/embed/9LPXqT-2tEc" // Проверенная ссылка
   },
+
+  // Дополнительные
   {
-    name: "EV3 Робот-манипулятор",
-    platform: "ev3",
-    motors: 3,
-    sensors: 3,
-    video: "https://www.youtube.com/embed/sQOY8Yq4k8o"
+    name: "Spike Танк",
+    platform: "spike",
+    motors: 4,
+    sensors: 2,
+    video: "https://www.youtube.com/embed/_EwpI7qJ5U8" // Проверенная ссылка
   },
   {
     name: "EV3 Лифт",
     platform: "ev3",
     motors: 3,
     sensors: 3,
-    video: "https://www.youtube.com/embed/oh5L4qz4Nbg"
-  },
-  // 10 роботов...
-  
-  // Добавим больше роботов, всего до 50
-  {
-    name: "EV3 Подъемник",
-    platform: "ev3",
-    motors: 2,
-    sensors: 1,
-    video: "https://www.youtube.com/embed/9Im0yxFz9T4"
-  },
-  {
-    name: "Spike Погрузчик",
-    platform: "spike",
-    motors: 3,
-    sensors: 2,
-    video: "https://www.youtube.com/embed/NYbIj1J8ytE"
-  },
-  {
-    name: "Spike Беспилотник",
-    platform: "spike",
-    motors: 4,
-    sensors: 3,
-    video: "https://www.youtube.com/embed/KdghPPE5XJw"
-  },
-  // Остальные роботы...
+    video: "https://www.youtube.com/embed/RdK80OpzodA" // Проверенная ссылка
+  }
 ];
+
 
 const form = document.getElementById("robotForm");
 const robotsContainer = document.getElementById("robotsContainer");
